7b102d7c23dffafbdf2289ca04c66ad8f30b36d8
unknown <48548611+supakjack@users.noreply.github.com>
2021-06-04 10:38:50 +0700

    update
    update

--- www/vue.config.js
+++ www/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   publicPath: '/info',
   outputDir: '../docs',
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].title = 'สุภัค พุกดำ'
-      return args
-    })
-  }
+  // chainWebpack: (config) => {
+  //   config.plugin('html').tap((args) => {
+  //     args[0].title = 'สุภัค พุกดำ'
+  //     return args
+  //   })
+  // }
 }
