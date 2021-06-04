e0ad0e63e1c0056dbe488adb8e089f9b70a59c98
unknown <48548611+supakjack@users.noreply.github.com>
2021-06-04 10:15:20 +0700

    change Title
    change Title

--- www/vue.config.js
+++ www/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath: '/info',
   outputDir: '../docs',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'สุภัค พุกดำ'
+      return args
+    })
+  }
 }
