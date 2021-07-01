9dd73d98a8c7b4acbd585dd32e32ac4573e46093
unknown <48548611+supakjack@users.noreply.github.com>
2021-07-01 20:51:43 +0700

    update page
    update page

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+$('.click_to_contact').click(function (e) {
+  e.preventDefault()
+  var url =
+    'https://www.facebook.com/%E0%B8%82%E0%B8%B2%E0%B8%A2%E0%B8%AA%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%88%E0%B9%84%E0%B8%AD%E0%B9%82%E0%B8%9F%E0%B8%99-%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%96%E0%B8%B9%E0%B8%81-1606080176293084'
+  window.open(url, '_blank').focus()
+})
